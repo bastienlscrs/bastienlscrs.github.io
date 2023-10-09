f266f84300ffd29061a1a8a800f8e02233d0ecf4
bastienlscrs <bastien.lscrs@eduge.ch>
2023-10-09 10:21:46 +0200

    add

--- sw.js
+++ sw.js
@@ -20,30 +20,6 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox
  */
 self.__precacheManifest = [
   {
-    "url": "index.html",
-    "revision": "87eecf4de281eb63d23398b95d11af80"
-  },
-  {
-    "url": "scripts/main.js",
-    "revision": "11590f3687a8296098e0980d744383f1"
-  },
-  {
-    "url": "scripts/utils.js",
-    "revision": "48b063f26eaf52a32f18929ff84978e7"
-  },
-  {
-    "url": "data/helloworld.json",
-    "revision": "18e2041efb50449379decffb0eac60ee"
-  },
-  {
-    "url": "styles/main.css",
-    "revision": "40a40e6967633a53e5c095675109aaeb"
-  },
-  {
-    "url": "styles/normalize.css",
-    "revision": "51e4e5bb1475d193fc34b28e93929bb6"
-  },
-  {
     "url": "images/icons/ic_info_outline_black_24px.svg",
     "revision": "3b1d479abe000b3ee42b1e12ff4e6003"
   },
