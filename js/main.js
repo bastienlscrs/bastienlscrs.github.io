f8ac51bc2998b95b80ffb762b45b1f1261ba866b
bastienlscrs <bastien.lscrs@eduge.ch>
2023-10-09 10:14:15 +0200

    tri

--- js/main.js
+++ js/main.js
@@ -4,21 +4,25 @@ let main = document.getElementById("main");
 JSON.stringify(url)
 localStorage.setItem('links', JSON.stringify(url));
 
-const stockedURL = JSON.parse(localStorage.getItem('links'));
+let stockedURL = JSON.parse(localStorage.getItem('links'));
 
 /**
  * Fonction qui stocke la list des pays dans le localStorage et Affiche
  */
 async function AllCountry(){
-    let data = await fetch(stockedURL);
+    let data = await fetch(stockedURL)
     let country = await data.json();
+    country.sort((a,b) => {
+        var nameA = a.translations.fra.common.toLowerCase();
+        var nameB = b.translations.fra.common.toLowerCase();
+
+        return (nameA < nameB) ? -1: (nameA > nameB) ? 1 : 0;
+    })
     localStorage.setItem('pays', JSON.stringify(country));
-    const stockedCountry = JSON.parse(localStorage.getItem('pays'));
+    let stockedCountry = JSON.parse(localStorage.getItem('pays'));
 
-    for (const c of stockedCountry) {
-
+    for (let c of stockedCountry) {
         main.innerHTML += `<p> ${c.name.common}</p> <img src="${c.flags.svg}" style="width: 200px;">`;
     }
 }
-
 AllCountry();
